a192881f6da318337e631ffd457429bce43b8db2
Alina <alina@Alinas-iMac.local>
2017-12-07 11:06:18 -0800

    created tab controller with two pages, first - map, second - weather

---
+++ TestApp/Controllers/SecondViewController.swift
@@ -0,0 +1,85 @@
+//
+//  SecondViewController.swift
+//  TestApp
+//
+//  Created by Alina on 12/6/17.
+//  Copyright © 2017 Alina. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    var forecastData = [Weather]()
+    
+    @IBAction func LogOutButtonTipped(_ sender: UIBarButtonItem) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateWeatherForLocation(location: "Ivano-Frankivsk")
+    }
+    
+    func updateWeatherForLocation (location: String) {
+        CLGeocoder().geocodeAddressString(location) { (placemarks:[CLPlacemark]?, error:Error?) in
+            if error == nil {
+                if let location = placemarks?.first?.location {
+                    Weather.forecast(withLocation: location.coordinate, completion: { (results:[Weather]?) in
+                        
+                        if let weatherData = results {
+                            self.forecastData = weatherData
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
+                        }
+                    })
+                }
+            }
+        }
+    }
+
+}
+
+extension SecondViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return forecastData.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let date = Calendar.current.date(byAdding: .day, value: section, to: Date())
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM dd, yyyy"
+        
+        return dateFormatter.string(from: date!)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        let weatherObject = forecastData[indexPath.section]
+        
+        cell.textLabel?.text = weatherObject.summary
+        cell.detailTextLabel?.text = "\(Int((weatherObject.temperature - 32) * 5/9)) °C"
+        cell.imageView?.image = UIImage(named: weatherObject.icon)
+        
+        return cell
+    }
+}
+
+extension SecondViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        if let locationString = searchBar.text, !locationString.isEmpty {
+            updateWeatherForLocation(location: locationString)
+        }
+        
+    }
+}
+
+
